744d438b1b839dd809b5f891a882af8a32675eb7
angus croll <anguscroll@gmail.com>
2011-07-08 15:52:42 -0700

    remove minified code

--- src/finder.js
+++ src/finder.js
@@ -28,4 +28,3 @@ javascript:(function(){
   }
 })();
 
-javascript:(function(){var a=function(d,f,c,g){var c=c||window;var g=g||((c==window)?"window":"");var e=Object.keys(c);e.forEach(function(h){if(b[d](c,f,h)){console.log([g,".",h].join(""),"->",["(",typeof c[h],")"].join(""),c[h])}if((""+c[h])=="[object Object]"&&g.split(".").indexOf(h)==-1){a(d,f,c[h],[g,h].join("."))}})};var b={p:function(c,d,e){return d==e},i:function(c,d,e){return c[e] instanceof d}};window.find={props:function(d,c,e){(typeof d=="string")&&a("p",d,c,e)},instances:function(d,c,e){(typeof d=="function")&&a("i",d,c,e)}}})();

ef81e42f07c505457b1408a4ed9d55b46bdad0e2
John-David Dalton <john.david.dalton@gmail.com>
2011-07-11 12:39:58 -0400

    waldo: Make work in IE8 & avoid stack overflows. [jddalton]

--- src/finder.js
+++ src/finder.js
@@ -1,39 +1,71 @@
-(function(){
-  var traverse = function(util, searchTerm, options) {
-    var options = options || {};
-    var obj = options.obj || window;
-    var path = options.path || ((obj==window) ? "window" : "");
-    var props = Object.keys(obj);
-    props.forEach(function(prop) {
-      if ((tests[util] || util)(searchTerm, obj, prop)){
-        console.log([path, ".", prop].join(""), "->",["(", typeof obj[prop], ")"].join(""), obj[prop]);
-      }
-      if((""+obj[prop])=="[object Object]" && (obj[prop] != obj) && path.split(".").indexOf(prop) == -1) {
-        traverse(util, searchTerm, {obj: obj[prop], path: [path,prop].join(".")});
-      }
-    });
+(function(window) {
+
+  function dealWithIt(util, expected, searchTerm, options) {
+    (!expected || typeof searchTerm == expected)
+      ? traverse(util, searchTerm, options)
+      : console.error(searchTerm + ' must be ' + expected);
   }
 
-  var dealWithIt = function(util, expected, searchTerm, options) {
-    (!expected || typeof searchTerm == expected) ?
-      traverse(util, searchTerm, options) :
-      console.error([searchTerm, 'must be', expected].join(' '));
+  function traverse(util, searchTerm, options) {
+    options || (options = {});
+    var pair;
+    var obj = options.obj || window;
+    var ownProp = options.hasOwnProperty;
+    var path = options.path || ((obj == window) ? 'window' : '');
+    var pairs = [[obj, path]];
+
+    // a non-recursive solution to avoid stack overflows
+    // http://www.jslab.dk/articles/non.recursive.preorder.traversal.part4
+    while ((pair = pairs.pop())) {
+      obj = pair[0];
+      path = pair[1];
+      for (var prop in obj) {
+        // IE may throw errors when accessing/coercing some properties
+        try {
+          if (ownProp.call(obj, prop)) {
+            if ((tests[util] || util)(searchTerm, obj, prop)){
+              console.log(path + '.' + prop, '->', '(' + (typeof obj[prop]) + ')', obj[prop]);
+              return;
+            }
+            if([obj[prop]] == '[object Object]' && (obj[prop] != obj)) {
+              pairs.push([obj[prop], path + '.' + prop]);
+            }
+          }
+        } catch(e) { }
+      }
+    }
   }
 
   var tests = {
-    'propName': function(searchTerm, obj, prop) {return searchTerm == prop},
-    'type': function(searchTerm, obj, prop) {return obj[prop] instanceof searchTerm},
-    'value': function(searchTerm, obj, prop) {return obj[prop] === searchTerm},
-    'valueCoerced': function(searchTerm, obj, prop) {return obj[prop] == searchTerm}
-  }
+    'propName': function(searchTerm, obj, prop) {
+      return searchTerm == prop;
+    },
+    'type': function(searchTerm, obj, prop) {
+      return obj[prop] instanceof searchTerm;
+    },
+    'value': function(searchTerm, obj, prop) {
+      return obj[prop] === searchTerm;
+    },
+    'valueCoerced': function(searchTerm, obj, prop) {
+      return obj[prop] == searchTerm;
+    }
+  };
 
-  window.find={
-    byPropName: function(searchTerm, options) {dealWithIt('propName', 'string', searchTerm, options);},
-    byType: function(searchTerm, options) {dealWithIt('type', 'function', searchTerm, options);},
-    byValue: function(searchTerm, options) {dealWithIt('value', null, searchTerm, options);},
-    byValueCoerced: function(searchTerm, options) {dealWithIt('valueCoerced', null, searchTerm, options);},
-    custom: function(fn, options) {traverse(fn, null, options);}
-  }
-})();
-
-
+  window.find = {
+    'byPropName': function(searchTerm, options) {
+      dealWithIt('propName', 'string', searchTerm, options);
+    },
+    'byType': function(searchTerm, options) {
+      dealWithIt('type', 'function', searchTerm, options);
+    },
+    'byValue': function(searchTerm, options) {
+      dealWithIt('value', null, searchTerm, options);
+    },
+    'byValueCoerced': function(searchTerm, options) {
+      dealWithIt('valueCoerced', null, searchTerm, options);
+    },
+    'custom': function(fn, options) {
+      traverse(fn, null, options);
+    }
+  };
+}(this));

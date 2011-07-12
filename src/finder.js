2c27a92b813eb5dfbedc02a337ef289ea2e49ef9
John-David Dalton <john.david.dalton@gmail.com>
2011-07-11 20:32:23 -0400

    waldo: Add comments, rework to detect multiple props and avoid circular references. [jddalton]

--- src/finder.js
+++ src/finder.js
@@ -1,34 +1,55 @@
 (function(window) {
 
   function dealWithIt(util, expected, searchTerm, options) {
+    // integrity check arguments
     (!expected || typeof searchTerm == expected)
       ? traverse(util, searchTerm, options)
       : console.error(searchTerm + ' must be ' + expected);
   }
 
   function traverse(util, searchTerm, options) {
+    util = tests[util] || util;
     options || (options = {});
-    var pair;
+
+    var data;
+    var pool;
+    var pooled;
+    var prevPath;
+
     var obj = options.obj || window;
     var ownProp = options.hasOwnProperty;
     var path = options.path || ((obj == window) ? 'window' : '');
-    var pairs = [[obj, path]];
+    var queue = [{ 'obj': obj, 'path': path }];
 
-    // a non-recursive solution to avoid stack overflows
+    // a non-recursive solution to avoid call stack limits
     // http://www.jslab.dk/articles/non.recursive.preorder.traversal.part4
-    while ((pair = pairs.pop())) {
-      obj = pair[0];
-      path = pair[1];
+    while ((data = queue.pop())) {
+      obj = data.obj;
+      path = data.path;
+      pooled = false;
+
+      // clear pool when switching root properties
+      path.indexOf(prevPath) && (pool = [data]);
+      prevPath = path;
+
       for (var prop in obj) {
         // IE may throw errors when accessing/coercing some properties
         try {
           if (ownProp.call(obj, prop)) {
-            if ((tests[util] || util)(searchTerm, obj, prop)){
-              console.log(path + '.' + prop, '->', '(' + (typeof obj[prop]) + ')', obj[prop]);
-              return;
+            // inspect objects
+            if ([obj[prop]] == '[object Object]') {
+              // check if already pooled (prevents circular references)
+              for (var i = -1; pool[++i] && !(pooled = pool[i].obj == obj[prop] && pool[i]); ) { }
+              // add to stack
+              if (!pooled) {
+                data = { 'obj': obj[prop], 'path': path + '.' + prop };
+                pool.push(data);
+                queue.push(data);
+              }
             }
-            if([obj[prop]] == '[object Object]' && (obj[prop] != obj)) {
-              pairs.push([obj[prop], path + '.' + prop]);
+            // if match detected, log it
+            if (util(searchTerm, obj, prop)) {
+              console.log(path + '.' + prop, '->', '(' + (pooled ? '<' + pooled.path + '>' : typeof obj[prop]) + ')', obj[prop]);
             }
           }
         } catch(e) { }
@@ -51,6 +72,7 @@
     }
   };
 
+  // expose
   window.find = {
     'byPropName': function(searchTerm, options) {
       dealWithIt('propName', 'string', searchTerm, options);

c32f3f4d517574e6f3f0ce5a2a322af50a6db38c
angus croll <angus@twitter.com>
2015-06-03 08:36:28 -0700

    clean up spec

--- test/finder_spec.js
+++ test/finder_spec.js
@@ -1,90 +1,106 @@
-import findES6 from '../lib/finder.es6.js';
-let findMax = require ('../lib/finder');
-let findMin = require ('../lib/finder.min');
+import maxified from '../lib/finder';
+import minified from '../lib/finder.min';
+import es6ified from '../lib/finder.es6.js';
 
-//dummy objects
-window.testObj = {
+const global = window || global;
+
+// dummy objects
+global.testObj = {
   obj: {d: 4},
   arr1: [1, 2, 3, 4, 5],
   arr2: ['a', 'b', 'c'],
-  fn: function() {},
+  fn: function () {},
   num: 1
 }
 
-window.testObj.circ = {a:3, b: window.testObj.obj};
-var logSpy;
+global.testObj.circ = {a: 3, b: global.testObj.obj};
+let logSpy;
 
-[findMax, findMin, findES6].forEach(find => {
-  describe("waldo", function() {
-    beforeEach(function() {
+[maxified, minified, es6ified].forEach(find => {
+  describe('waldo', () => {
+    beforeEach(() => {
       logSpy = spyOn(console, 'log').and.callThrough();
     });
-    describe("findByName", function() {
-      it("should find root level object", function() {
+    describe('findByName', () => {
+      it('should find root level object', () => {
         find.byName('circ');
-        expect(console.log).toHaveBeenCalledWith(`global.testObj.circ -> (object) ${window.testObj.circ}`);
+        expect(console.log).toHaveBeenCalledWith(
+          `global.testObj.circ -> (object) ${global.testObj.circ}`);
       });
-      it("should find root level array", function() {
+      it('should find root level array', () => {
         find.byName('arr1');
-        expect(console.log).toHaveBeenCalledWith(`global.testObj.arr1 -> (object) ${window.testObj.arr1}`);
+        expect(console.log).toHaveBeenCalledWith(
+          `global.testObj.arr1 -> (object) ${global.testObj.arr1}`);
       });
-      it("should find nested property", function() {
+      it('should find nested property', () => {
         find.byName('a');
-        expect(console.log).toHaveBeenCalledWith('global.testObj.circ.a -> (number) 3');
+        expect(console.log).toHaveBeenCalledWith(
+          'global.testObj.circ.a -> (number) 3');
       });
-      it("should detect circular reference", function() {
+      it('should detect circular references', () => {
         find.byName('d');
-        expect(console.log).toHaveBeenCalledWith('global.testObj.obj.d -> (<global.testObj.obj>) 4');
+        expect(console.log).toHaveBeenCalledWith(
+          'global.testObj.obj.d -> (<global.testObj.obj>) 4');
       });
     });
-    describe("findByType", function() {
-      it("should find first class objects types", function() {
-        find.byType(Array, {obj: window.testObj, path: 'testObj'});
-        //TODO need to check for multiple matches
-        expect(console.log).toHaveBeenCalledWith(`testObj.arr1 -> (object) ${window.testObj.arr1}`);
+
+    describe('findByType', () => {
+      it('should find first class objects types', () => {
+        find.byType(Array, {obj: global.testObj, path: 'testObj'});
+        // TODO need to check for multiple matches
+        expect(console.log).toHaveBeenCalledWith(
+          `testObj.arr1 -> (object) ${global.testObj.arr1}`);
         logSpy.calls.reset();
-        find.byType(Function, {obj: window.testObj, path: 'testObj'});
-        expect(console.log).toHaveBeenCalledWith(`testObj.fn -> (function) ${window.testObj.fn}`);
+        find.byType(Function, {obj: global.testObj, path: 'testObj'});
+        expect(console.log).toHaveBeenCalledWith(
+          `testObj.fn -> (function) ${global.testObj.fn}`);
       });
-      it("should not find primitive types", function() {
-        find.byType(String, {obj: window.testObj, path: 'testObj'});
+      it('should not find primitive types', () => {
+        find.byType(String, {obj: global.testObj, path: 'testObj'});
         expect(console.log).not.toHaveBeenCalled();
       });
     });
-    describe("findByValue", function() {
-      it("should find number", function() {
-        find.byValue(3, {obj: window.testObj, path: 'testObj'});
-        expect(console.log).toHaveBeenCalledWith('testObj.circ.a -> (number) 3');
+
+    describe('findByValue', () => {
+      it('should find number', () => {
+        find.byValue(3, {obj: global.testObj, path: 'testObj'});
+        expect(console.log).toHaveBeenCalledWith(
+          'testObj.circ.a -> (number) 3');
       });
-      it("should find number and detect circular reference", function() {
-        find.byValue(4, {obj: window.testObj, path: 'testObj'});
-        expect(console.log).toHaveBeenCalledWith('testObj.obj.d -> (<testObj.obj>) 4');
+      it('should find number and detect circular reference', () => {
+        find.byValue(4, {obj: global.testObj, path: 'testObj'});
+        expect(console.log).toHaveBeenCalledWith(
+          'testObj.obj.d -> (<testObj.obj>) 4');
       });
-      it("should find complex value", function() {
-        find.byValue(window.testObj.arr2, {obj: window.testObj, path: 'testObj'});
-        expect(console.log).toHaveBeenCalledWith(`testObj.arr2 -> (object) ${testObj.arr2}`);
-      });
-    });
-    describe("findByValueCoreced", function() {
-      it("should find number equivalent of a string", function() {
-        find.byValueCoerced('3', {obj: window.testObj, path: 'testObj'});
-        expect(console.log).toHaveBeenCalledWith('testObj.circ.a -> (number) 3');
-      });
-      it("should not find falsey values when non exist", function() {
-        find.byValueCoerced(false, {obj: window.testObj, path: 'testObj'});
-        expect(console.log).not.toHaveBeenCalledWith();
+      it('should find complex value', () => {
+        find.byValue(global.testObj.arr2, {obj: global.testObj, path: 'testObj'});
+        expect(console.log).toHaveBeenCalledWith(
+          `testObj.arr2 -> (object) ${global.testObj.arr2}`);
       });
     });
-    describe("findByCustomeFilter", function() {
-      it("should return custom filter matches", function() {
-        find.custom(function(searchTerm, obj, prop) {return (obj[prop] === 1) && (prop == 'num')});
+
+    describe('findByValueCoreced', () => {
+      it('should find number equivalent of a string', () => {
+        find.byValueCoerced('3', {obj: global.testObj, path: 'testObj'});
+        expect(console.log).toHaveBeenCalledWith(
+          'testObj.circ.a -> (number) 3');
+      });
+      it('should not find falsey values when non exist', () => {
+        find.byValueCoerced(false, {obj: global.testObj, path: 'testObj'});
+        expect(console.log).not.toHaveBeenCalled();
+      });
+    });
+
+    describe('findByCustomeFilter', () => {
+      it('should return custom filter matches', () => {
+        find.custom((searchTerm, obj, prop) => (obj[prop] === 1) && (prop == 'num'));
         expect(console.log).toHaveBeenCalledWith('global.testObj.num -> (number) 1');
       });
-      it("should report no matches when no custom filter matches", function() {
-        find.custom(function(searchTerm, obj, prop) {return (obj[prop] === 1) && (prop == 'pie')});
+      it('should report no matches when no custom filter matches', () => {
+        find.custom((searchTerm, obj, prop) => (obj[prop] === 1) && (prop == 'pie'));
         expect(console.log).not.toHaveBeenCalled();
       });
-      //TODO: test searchTerm param
+      // TODO: test searchTerm param
     });
   });
 });
